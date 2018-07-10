1a6202572210571cb78203f75a0b844a15c30e80
Mandy-PC\Mandy <mtwmt@hotmail.com>
2018-07-11 02:24:24 +0800

    validate v2.0

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# 安裝  
+
+```npm install```
+
 # framework
-### [前端驗証 validate](https://mtwmt.github.io/framework/public/validate.html)
-### [滑到錨點位置 gohref](https://mtwmt.github.io/framework/public/gohref.html)
+
+* [前端驗証 validate](https://mtwmt.github.io/framework/public/validate.html)  
+* [滑到錨點位置 gohref](https://mtwmt.github.io/framework/public/gohref.html)
--- sourse/js/validate.js
+++ sourse/js/validate.js
@@ -1,92 +1,94 @@
-var validate = validate || {};
+(function( $ ) {
+  $.fn.validate = function( options ) {
+    
+    options = $.extend({
+      success: function(){ alert('success'); },
+      error: function(){},
+      errcls: 'err-msg'
+    }, options);
 
-validate = (function($){
-
-  var $form = $('[data-form]'),
-      $submit = $('[data-submit]'),
-      cls,
-      log,
-      check = {
-        name: function(){
-          return RegExp(/^[a-zA-Z0-9\u4e00-\u9fa5]{1,7}$/);
-        },
-        mail: function(){
-          return RegExp(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/);
-        },
-        mobile: function(){
-          return RegExp(/^09[0-9]{8}$/);
-        },
-        password: function(dom, min, max){
-          var min = min || '',
-              max = max || '';
-          return RegExp(`[a-zA-Z0-9_-]{${min},${max}}$`);
-        },
-        same: function( dom ){
-          var target = dom.data('same'),
-              $target = $('.'+ target +'');
-              console.log( $target.find('input').val() );
-          return RegExp(`${$target.find('input').val()}`);
-        }
+    var
+    $form = $(this),
+    $verify = $('[data-verify]'),
+    $submit = $('[data-submit]'),
+    check = {
+      name: function(){
+        return RegExp(/^[a-zA-Z0-9\u4e00-\u9fa5]{1,7}$/);
       },
-  chkErr = function($select,msg){
-    $select.addClass('error');
-    if( !$select.is('[data-msg]') ){
-      $select.find('[data-msg]').append(msg);
-    }else{
-      $select.append(`<em class="err-msg">${msg}</em>`);
-    }
-  },
-  chk = function( $select, obj, msg ){
-    var str,
-        newStr,
-        minLen = $select.data('min') || '',
-        maxLen = $select.data('max') || '';
-        $select.removeClass('error');
-
-        if( !$select.is('[data-msg]') ){
-          str = $select.find('[data-msg]').text();
-          newStr = str.replace(msg ,'');
-          $select.find('[data-msg]').text(newStr);
-        }else{
-          $select.find('.err-msg').remove('.err-msg');
-        }
-    if( !check[obj]( $select, minLen, maxLen ).test( $select.find('input').val() ) ){
-      chkErr( $select,msg );
-    }
-  },
+      mail: function(){
+        return RegExp(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/);
+      },
+      mobile: function(){
+        return RegExp(/^09[0-9]{8}$/);
+      },
+      password: function(dom, min, max){
+        var min = min || '',
+            max = max || '';
+        return RegExp(`[a-zA-Z0-9_-]{${min},${max}}$`);
+      },
+      same: function( dom ){
+        var target = dom.data('same'),
+            $target = $('.'+ target +'');
+        return RegExp(`${$target.find('input').val()}`);
+      }
+    },
+    chkErr = function($select,msg){
+      $select.addClass('error');
+      if( !$select.is('[data-msg]') ){
+        $select.find('[data-msg]').append(msg);
+      }else{
+        $select.append(`<em class="${options.errcls}">${msg}</em>`);
+      }
+    },
+    chk = function( $select, obj, msg ){
+      var str,
+          newStr,
+          minLen = $select.data('min') || '',
+          maxLen = $select.data('max') || '';
+          $select.removeClass('error');
   
-  init = function(){
-    $.each( check,function(i, e){
-      $(`[data-${i}]`).each(function(idx,el){
-        var $input = $(this).find('input'),
-            msg = $(this).find('[data-msg]').data('msg') || $(this).data('msg');
-        $input.on('blur',function(){
-          chk( $(el),i ,msg);
-        });
-      });
-    });
-    $submit.on('click',function(){
+          if( !$select.is('[data-msg]') ){
+            str = $select.find('[data-msg]').text();
+            newStr = str.replace(msg ,'');
+            $select.find('[data-msg]').text(newStr);
+
+            
+          }else{
+            $select.find('.'+ options.errcls +'').remove('.'+ options.errcls +'');
+          }
+      if( !check[obj]( $select, minLen, maxLen ).test( $select.find('input').val() ) ){
+        chkErr( $select,msg );
+      }
+    },
+    actValidate = function(){
+      var $self = $(this),
+          log = [],
+          msg = $self.data('msg') || $self.find('[data-msg]').data('msg');
+      for( var i in check ){
+        if( i.indexOf($self.data('verify')) >= 0 ){
+          chk( $self, i ,msg);
+        }
+      }
+    },
+    actSubmit = function(){
       var log = [];
-      $.each(check,function(i,e){
-        $(`[data-${i}]`).each(function(idx, el){
-          console.log(i, $(this).find('input').val());
+      $verify.each(function( i,e ){
           if($(this).hasClass('error') || !$(this).find('input').val()){
             $(this).addClass('error');
             log.push('err');
           }
-        });
       });
-      if( (log.length) != 0 ){
-        alert('error');
+      if( !(log.length) ){
+        $(this).one('success',options.success).trigger('success');
+        
       }else{
-        alert('success');
+        $(this).one('error',options.error).trigger('error');
       }
+    };
+    $verify.on('test',actValidate).on('blur','input',function(){
+      $(this).trigger('test');
     });
-  }
-
-  return{
-    init: init
+    $submit.on('click',actSubmit)
   };
-})(jQuery);
 
-validate.init();
+}( jQuery ));
